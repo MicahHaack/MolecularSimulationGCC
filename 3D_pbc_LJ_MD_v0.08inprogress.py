494c563a6c94633ba84e208e2d4a0c318bd0607a
Micah Haack <44842363+MicahHaack@users.noreply.github.com>
2019-04-22 13:07:33 -0400

    removed all global values and worked on verlet neighbor list algos

--- 3D_pbc_LJ_MD_v0.08inprogress.py
+++ 3D_pbc_LJ_MD_v0.08inprogress.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Feb  3 16:38:54 2019
-@author: Kel
+@author: Micah Haack
 
 TODO
-remove globals
+remove globals DONE!!! SUCCESSFULLY COMPLETED YAY
 numba and JIT compiler to try and speed up loops
 neighbor list algorithm
 algo 37 & 40
 
 ref 530
-backup code to GitHub
+backup code to GitHub : repository made, no spyder git integration, manual backup needed
 """
 
 import numpy as np
@@ -20,64 +20,82 @@ import matplotlib.pyplot as plt
 from numba import jit
 import time
 
-# matplotlib
-
-
-#GLOBALS
-num_atoms = 125 #was 125
-box_dim = 7
-time_step = 0.005
-temp = 0.728
-vel = 0
-pos_pre = 0
-pos = 0
-cutoff = float(box_dim / 2) - 0.1  # should be less than half the box dimensions
-ngr = 0
-delg = 0
-nhis = 80 # number of histograms
-rho = 0.8442
-
-g = 0
-
-numiter = 100
-
-evals = np.zeros(numiter)
-tvals = np.zeros(numiter)
-kvals = np.zeros(numiter)
-etotvals = np.zeros(numiter)
-
-# initially set forces to zero
-force = np.zeros(( num_atoms, 3))
-energy = 0.0
-
 def main():
     
-    global tvals
-    global kvals 
-    global evals
-    global etotvals
+    # Initial Values
+    num_atoms = 125
+    box_dim = 7
+    time_step = 0.005
+    temp = 0.728
+    cutoff = float(box_dim / 2) - 0.1 # should be < 1/2 the box dims
+    nhis = 80 # number of histograms
+    rho = 0.8442
+    numiter = 100
+    force = np.zeros(( num_atoms, 3))
+    energy = 0.0
+    ncells= 27 # b/c 3D system?
     
-    init()
+    rc = 2.5 # * sigma? these are values from the book but I don't remember
+    rv = 2.7 # * sigma  what sigma was in terms for reduced units -> maybe 1?
+    
+    # data storage for values to graph
+    tvals = np.zeros(numiter)
+    kvals = np.zeros(numiter)
+    evals = np.zeros(numiter)
+    etotvals = np.zeros(numiter)
+    
+    # package variable for function input
+    package = [num_atoms, box_dim, temp, time_step, nhis]
+    
+    # out variable for function output
+    #out = [pos, pos_pre, sumv, sumv2, vel, ngr, delg, g]
+    out = init(package)
+    
+    # UNPACK VALS FROM init()
+    pos = out[0]
+    pos_pre = out[1]
+    #sumv = out[2]
+    #sumv2 = out[3] unused? ^ v
+    #vel = out[4]
+    ngr = out[5]
+    delg = out[6]
+    g = out[7]
     
     for i in range(0, numiter):
     
         tvals[i] = time_step*i
+
+        # PACK VALUES FOR force_calc()
+        package = [num_atoms, pos, box_dim, cutoff, ngr, delg, g]
+
+        out = force_calc(package)
+        # unpack output from force_calc()
+        force = out[0]
+        energy = out[1]
+        ngr = out[2]
         
-        #print(i, "before", energy)
-        #force_calc()
-        #print(i, "after", energy)
-        force_calc()
-    
-        etot, k = integ()
+        # PACK VALUES FOR integ()        
+        package = [num_atoms, pos, pos_pre, force, energy, time_step, temp]
+        
+        out = integ(package)
+        # unpack output from integ()
+        etot = out[0]
+        k = out[1]
+        temp = out[2]
         
         evals[i] = energy / num_atoms
         etotvals[i] = etot
         kvals[i] = 0.5 * (k[0] + k[1] + k[2]) / num_atoms
         
-        rvals = algo7_radial()
+        # PACK VALUES FOR algo7_radial()
+        package = [g, nhis, delg, rho, num_atoms, ngr]
+        out = algo7_radial(package)
+        # unpack output from algo7_radial()
+        r = out[0]
+        g = out[1]
         
-    
-    #print(etotvals)
+        
+    # Begin plots
     
     plt.plot(tvals, etotvals, label='E Total')
     plt.plot(tvals, kvals, label='Kinetic Energy')
@@ -91,7 +109,7 @@ def main():
     
     plt.show()
     
-    plt.plot(rvals, g, label='Distribution')
+    plt.plot(r, g, label='Distribution')
     plt.xlabel('r vals')
     plt.ylabel('g')
     
@@ -100,92 +118,109 @@ def main():
     
     plt.show()
     
-def algo7_radial():
     
-    global g
+    
+def verlet_algo40(package):
+    
+    # unpack inputs
+    box_dim = package[0]
+    num_atoms = package[1]
+    rc = package[2]
+    ncells = package[3]
+    pos = package[4]
+    
+    # make cell lists?
+    out = cell_algo37([box_dim, num_atoms, rc, ncells, pos])
+    hoc = out[0]
+    ll = out[1]
+    rn = out[2]
+    
+    # prep the nlist
+    nlist = np.zeros(num_atoms)
+    # do we not need to store the pos of particles? xv
+    # or is this just a copy of the pos array?
+    
+    for i in range(1, num_atoms):
+        
+        # determine cell number
+        icel = int(round(pos[i] / rn))
+        
+        #for j in range(1, ) # wait what is neigh?
+    
+    
+    
+def cell_algo37(package):
+    
+    box_dim = package[0]
+    num_atoms = package[1]
+    rc = package[2] # size of the cells
+    ncells = package[3]
+    pos = package[4]
+    
+    # prep the LL array    
+    ll = np.zeros(num_atoms)
+    
+    hoc = 0 # this will be the head of chain array
+    
+    # determine size of cells rn >= rc
+    rn = box_dim / int(round(box_dim/rc))
+    
+    # init the hoc array with zeros
+    hoc = np.zeros(ncells - 1)
+    
+    # loop over particles
+    for i in range(1, num_atoms):
+        
+        # determine the cell number
+        icel = int(round(pos[i] / rn))
+        
+        # link list the hoc of cell icel
+        ll[i] = hoc[icel]
+        
+        # make particle i the head of chain
+        hoc[icel] = i
+    
+    out = [hoc, ll, rn]
+    return out
+    
+    
+def algo7_radial(package):
+    
+    g = package[0]
+    nhis = package[1]
+    delg = package[2]
+    rho = package[3]
+    num_atoms = package[4]
+    ngr = package[5]
     
     r = np.zeros(nhis)
     for i in range(0, nhis):
         
         r[i] = delg * (i + 0.5) # distance r
-        vb = (( (i+1) * (i+1) * (i+1) ) - (i*i*i)) * (delg*delg*delg) #volume between bin i+1 and i?
+        vb = (( (i+1) * (i+1) * (i+1) ) - (i*i*i)) * (delg*delg*delg) # volume between bin i+1 and i?
         nid = (4/3)*(math.pi)*vb*rho #number of ideal gas particles in vb -> what is rho -> density
         g[i] = g[i] / (ngr*num_atoms*nid)
     
-    return r
+    out = [r, g]
     
+    return out
 
-def new_force_calc():
-
-    global num_atoms
-    global pos
-    global pos_pre
-    global sumv
-    global sumv2
-    global vel
-    global force
-    global box_dim
-    global energy
-    global ngr
+def force_calc(package):
     
-    energy = 0
-    force = np.zeros(( num_atoms, 3))
-    ecut = 4 * ( math.pow((1 / cutoff), 12) - math.pow((1 / cutoff), 6) )
-    ngr = ngr + 1
-    
-        # start simple and just try to replace one loop
-    for i in range(0, num_atoms - 1):
-        
-        int_arr = (np.round(pos / box_dim)).astype(int)
-        diffvector = pos[i] - (pos - box_dim * int_arr)
-        
-        d2_arr = np.sum(diffvector*diffvector, axis=1)
-        distance_arr= d2_arr/2
-        
-        # now try a boolean masking array
-        #mask_arr = distance_arr < box_dim/2
-        # skip radial calc for now
-        
-        mask_arr = d2_arr < cutoff*cutoff
-        new_pos_arr = pos[mask_arr]
-        
-        r2i_arr = 1 / d2_arr[mask_arr]
-        r6i_arr = r2i_arr * r2i_arr * r2i_arr
-        ff_arr = 48 * r2i_arr * r6i_arr * (r6i_arr - 0.5)
-        
-        # update forces?
-        
-        diffvector = diffvector[mask_arr]
-        ff_arr_multi = np.repeat(ff_arr[0], np.size(diffvector, axis=0)*3).reshape((np.size(diffvector, axis=0), 3))
-        
-        temp = ff_arr_multi * diffvector
-        
-        # not sure how to update the force
-        
-        energy = energy + 4 * np.sum(r6i_arr) * (np.sum(r6i_arr - 1)) - ecut
-
-def force_calc():
-    
-    global num_atoms
-    global pos
-    global pos_pre
-    global sumv
-    global sumv2
-    global vel
-    global force
-    global box_dim
-    global energy
-    global ngr
+    # unpack inputs
+    num_atoms = package[0]
+    pos = package[1]
+    box_dim = package[2]    
+    cutoff = package[3]
+    ngr = package[4]
+    delg = package[5]
+    g = package[6]
     
     energy = 0
     force = np.zeros(( num_atoms, 3))
     ecut = 4 * ( math.pow((1 / cutoff), 12) - math.pow((1 / cutoff), 6) )
     # START FORCE LOOP
     
-    # not sure if this is corret -> need help writing this algorithm
-    #for x in np.nditer(pos):
-        #distances = np.subtract(x, pos)
-        #force = np.sum(distances[distances < cutoff])
     
     ngr = ngr + 1
     
@@ -209,14 +244,11 @@ def force_calc():
             diffy = diffy - box_dim * int(round(diffy / box_dim))
             diffz = diffz - box_dim * int(round(diffz / box_dim))
             
-            
-            #d2 = diffx * diffx + diffy * diffy + diffz * diffz  -> changed this to be the sqrt
             d2 = diffx * diffx + diffy * diffy + diffz * diffz
             
             distance = math.sqrt(d2)
             
             if distance < box_dim / 2:
-                
                 # pause for a second and do calculations for radial function ALGO7
                 ig = int(round(distance / delg)) - 1
                 g[ig] = g[ig] + 2
@@ -238,39 +270,26 @@ def force_calc():
             
                 # z
                 force[i][2] = force[i][2] + ff * diffz
-                force[j][2] = force[j][2] - ff * diffz
+                force[j][2] = force[j][2] - ff * diffz               
+                    
+                energy = energy + 4 * r6i * (r6i - 1) - ecut                
                 
-                #if force[i][0] > 10000:
-                    #print("diffx", diffx)
-                    #print("diffy", diffy)
-                    #print("diffz", diffz)
-                    
-                    
-                    #print(i, force[i][0], force[i][1], force[i][2])
-                    
-                    
-                energy = energy + 4 * r6i * (r6i - 1) - ecut
-                
-                # work on intergration routine
-                
-    #temp_f = np.sum(force, axis=0)
-    #print(temp_f)
+    out = [force, energy, ngr]
+    return out
     
     #return force, energy
 
 
-def integ():
+def integ(package):
     
-    global num_atoms
-    global pos
-    global pos_pre
-    global vel
-    global force
-    global box_dim
-    global energy
-    global time_step
-    global temp
-    
+    # unpack input
+    num_atoms = package[0]
+    pos = package[1]
+    pos_pre = package[2]
+    force = package[3]
+    energy = package[4]
+    time_step = package[5]
+    temp = package[6]
     
     # Algorithm 6
     
@@ -296,9 +315,7 @@ def integ():
         force_x = force[i][0]
         force_y = force[i][1]
         force_z = force[i][2]
-    
-        #print(i, "force", force_x, force_y, force_z) # some force values are getting rather large
-    
+        
         # run eq 4.2.3 for each x, y, z
         xx = 2 * atomx - atomx_pre + time_step * time_step * force_x
         yy = 2 * atomy - atomy_pre + time_step * time_step * force_y
@@ -309,59 +326,44 @@ def integ():
         vi_y = (yy - atomy_pre) / (2 * time_step)
         vi_z = (zz - atomz_pre) / (2 * time_step)
         
-        #print(i, vi_x, vi_y, vi_z)
         
         v_new_arr = np.array([vi_x, vi_y, vi_z])
         v2_new_arr = np.array([vi_x * vi_x, vi_y * vi_y, vi_z * vi_z])
 
-        #print(i, v_new_arr, v2_new_arr)
 
         # velocity center of mass
         sumv = sumv + v_new_arr
-        #print(i)
-        #print(v_new_arr)
-        #print(i, 'sumv ', sumv) # last iteration this goes back to basicially 0 (e-9), but in the middle (i = 8 to i = 19) this value gets very large
         # total kinetic energy
         sumv2 = sumv2 + v2_new_arr
-        #print(i, sumv2)
         
         # update positions of previous time
         pos_pre[i][0] = pos[i][0]
         pos_pre[i][1] = pos[i][1]
         pos_pre[i][2] = pos[i][2]
-        #print(i, "pos", pos[i][0], pos[i][1], pos[i][2]) # some particles get very large position values, but then the first and last particles in the list suddenly has
-        # to account for this, so their position values explode
         
         # update positions of current time
         pos[i][0] = xx
         pos[i][1] = yy
         pos[i][2] = zz
         
-    #sumv = np.sum(sumv, axis=0)
-    #sumv2 = np.sum(sumv*sumv, axis=0)
     
     # update temp val
     temp = (sumv2[0] + sumv2[1] + sumv2[2]) / (3 * num_atoms)
-    #print('sumv: ', sumv)
-    #print('temp: ', temp)
+
     # find total energy per particle
     etot = (energy + 0.5 * (sumv2[0] + sumv2[1] + sumv2[2])) / num_atoms
-    #print(sumv2)
+    
+    out = [etot, sumv2, temp]
         
-    return etot, sumv2
+    return out
       
-def init():
+def init(package):
     
-    
-    global num_atoms
-    global pos
-    global pos_pre
-    global sumv
-    global sumv2
-    global vel
-    global ngr
-    global delg
-    global g
+    num_atoms = package[0]   
+    box_dim = package[1]
+    temp = package[2]
+    time_step = package[3]
+    nhis = package[4]
     
     # GENERATE A LATTICE IN THE BOX
     
@@ -385,11 +387,7 @@ def init():
 
     # expand that row into x, y, and z to generate a list of points in 3-D
     #https://stackoverflow.com/questions/18253210/creating-a-numpy-array-of-3d-coordinates-from-three-1d-arrays
-    pos = np.vstack(np.meshgrid(loc, loc, loc)).reshape(3, -1).T
-    
-    #print(pos)
-    #print(len(pos)) - checked, does generate the proper number of points
-    
+    pos = np.vstack(np.meshgrid(loc, loc, loc)).reshape(3, -1).T    
     
     # GIVE EACH POINT A RANDOM VELOCITY VALUE
     vel = random.rand(num_atoms, 3) - 0.5
@@ -409,8 +407,7 @@ def init():
     
     # Set desired kinetic energy and set velocity center of mass to zero position previous time step
     vel = (vel - sumv) * fs
-    #sumv = np.sum(vel, axis=0)
-    #print(sumv)
+
     pos_pre = pos - vel * time_step
     
     
@@ -420,6 +417,9 @@ def init():
     delg = box_dim / (2 * nhis)
     
     g = np.zeros(nhis)
+    
+    out = [pos, pos_pre, sumv, sumv2, vel, ngr, delg, g]
+    return out
 
 start = time.time()
 main()
